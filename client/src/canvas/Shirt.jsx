e8f51b433529ad7533c93e12622b8328c1daa2c7
Idan Lev <idan.93lev@gmail.com>
2023-04-02 00:43:53 +0300

    finished Shirt

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { easing } from "maath";
 import { useSnapshot } from "valtio";
 import { useFrame } from "@react-three/fiber";
 import { Decal, useGLTF, useTexture } from "@react-three/drei";
+
 import state from "../store";
 
 const Shirt = () => {
@@ -11,7 +13,9 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
-  useFrame((state, delta) => easing.dampC(materials.lamber1.color, snap.color, 0.25, delta));
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
 
   const stateString = JSON.stringify(snap);
 
@@ -32,13 +36,14 @@ const Shirt = () => {
             map={fullTexture}
           />
         )}
+
         {snap.isLogoTexture && (
           <Decal
-            position={[0, 0.4, 0.15]}
+            position={[0, 0.04, 0.15]}
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-            map-anisotrophy={16}
+            map-anisotropy={16}
             depthTest={false}
             depthWrite={true}
           />
