24147c1f5511e4786274994fcc1dc4cd0e3c879a
Idan Lev <idan.93lev@gmail.com>
2023-04-02 00:42:45 +0300

    finished Shirt with errors

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -6,12 +6,17 @@ import state from "../store";
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb')
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
 
-  const logoTexture = useTexture(snap.logoDecal)
-  const fullTexture = useTexture(snap.fullDecal)
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
+  useFrame((state, delta) => easing.dampC(materials.lamber1.color, snap.color, 0.25, delta));
+
+  const stateString = JSON.stringify(snap);
+
   return (
-    <group>
+    <group key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
@@ -19,10 +24,28 @@ const Shirt = () => {
         material-roughness={1}
         dispose={null}
       >
-
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.4, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            map-anisotrophy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
       </mesh>
     </group>
-  )
-}
+  );
+};
 
-export default Shirt
+export default Shirt;
