b5cbceadd40c5d5a2ec28f86c2728d8773845049
Idan Lev <idan.93lev@gmail.com>
2023-04-03 23:59:04 +0300

    removed unused declaration for three.js

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,5 +1,4 @@
 import React, { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
