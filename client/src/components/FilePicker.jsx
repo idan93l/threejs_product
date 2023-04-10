eb2151cd3c2a51ec531cc1f137519a8007483c32
Idan Lev <idan.93lev@gmail.com>
2023-04-10 03:18:26 +0300

    added tailwind classnames to close button at FilePicker

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -4,6 +4,7 @@ import { IoClose } from "react-icons/io5";
 const FilePicker = ({ file, setFile, readFile, setActiveEditorTab }) => {
   return (
     <div className="filepicker-container">
+      <button className="absolute -top-7 -right-7 h-6 w-6 flex justify-center items-center rounded-full bg-red-600 text-2xl text-white border-2"></button>
       <div className="flex-1 flex flex-col">
         <input
           id="file-upload"
@@ -11,24 +12,24 @@ const FilePicker = ({ file, setFile, readFile, setActiveEditorTab }) => {
           accept="image/*"
           onChange={(e) => setFile(e.target.files[0])}
         />
-      <label htmlFor="file-upload" className="filepicker-label">
-        Upload File
-      </label>
-      <p className="mt-2 text-grey-500 text-xs truncate">
-        {file === '' ? "No file selected" : file.name}
-      </p>
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload File
+        </label>
+        <p className="mt-2 text-grey-500 text-xs truncate">
+          {file === "" ? "No file selected" : file.name}
+        </p>
       </div>
       <div className="mt-4 flex flex-wrap gap-3">
-        <CustomButton 
+        <CustomButton
           type="outline"
           title="Logo"
-          handleClick={() => readFile('logo')}
+          handleClick={() => readFile("logo")}
           customStyles="text-xs"
         />
-        <CustomButton 
+        <CustomButton
           type="filled"
           title="Full"
-          handleClick={() => readFile('full')}
+          handleClick={() => readFile("full")}
           customStyles="text-xs"
         />
       </div>
