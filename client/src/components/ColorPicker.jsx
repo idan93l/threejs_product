664e9abd1b6707ab572cca20005dc4ba76653ca5
Idan Lev <idan.93lev@gmail.com>
2023-04-10 03:12:48 +0300

    working close button on colorPicker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,14 +1,19 @@
 import { SketchPicker } from "react-color";
 import { useSnapshot } from "valtio";
-
+import { IoClose } from "react-icons/io5";
 import state from "../store";
 
-const ColorPicker = ({setActiveEditorTab}) => {
+const ColorPicker = ({ setActiveEditorTab }) => {
   const snap = useSnapshot(state);
 
   return (
     <div className="absolute left-full ml-3">
-      <button className="absolute -top-6 -right-4" onClick={() => setActiveEditorTab("")}>X</button>
+      <button
+        className="absolute -top-7 -right-7 h-6 w-6 flex justify-center items-center rounded-full bg-red-600 text-2xl text-white border-2"
+        onClick={() => setActiveEditorTab("")}
+      >
+        <IoClose />
+      </button>
       <SketchPicker
         color={snap.color}
         disableAlpha
