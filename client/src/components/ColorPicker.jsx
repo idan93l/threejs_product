bdc2ace0c0ebfc45ec83f3890a02034eb112412d
Idan Lev <idan.93lev@gmail.com>
2023-04-12 04:08:02 +0300

    added framer-motion imports to ColorPicker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,25 +1,28 @@
 import { SketchPicker } from "react-color";
 import { useSnapshot } from "valtio";
 import { IoClose } from "react-icons/io5";
+import { AnimatePresence, motion } from "framer-motion";
 import state from "../store";
 
 const ColorPicker = ({ setActiveEditorTab }) => {
   const snap = useSnapshot(state);
 
   return (
-    <div className="absolute left-full ml-3">
-      <button
-        className="absolute -top-7 -right-7 h-6 w-6 flex justify-center items-center rounded-full bg-red-600 text-2xl text-white border-2"
-        onClick={() => setActiveEditorTab("")}
+      <div
+        className="absolute left-full ml-3"
       >
-        <IoClose />
-      </button>
-      <SketchPicker
-        color={snap.color}
-        disableAlpha
-        onChange={(color) => (state.color = color.hex)}
-      />
-    </div>
+        <button
+          className="absolute -top-7 -right-7 h-6 w-6 flex justify-center items-center rounded-full bg-red-600 text-2xl text-white border-2"
+          onClick={() => setActiveEditorTab("")}
+        >
+          <IoClose />
+        </button>
+        <SketchPicker
+          color={snap.color}
+          disableAlpha
+          onChange={(color) => (state.color = color.hex)}
+        />
+      </div>
   );
 };
 
