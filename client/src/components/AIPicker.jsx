41e339b9d101cec3deae3182f680c401a5d3a9de
Idan Lev <idan.93lev@gmail.com>
2023-04-14 04:19:41 +0300

    added AnimatePresence at AIPicker jsx

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -11,45 +11,47 @@ const AIPicker = ({
   setActiveEditorTab,
 }) => {
   return (
-    <div className="aipicker-container ">
-      <button
-        className="absolute -top-7 -right-7 h-6 w-6 flex justify-center items-center rounded-full bg-red-600 text-2xl text-white border-2"
-        onClick={() => setActiveEditorTab("")}
-      >
-        <IoClose />
-      </button>
-      <textarea
-        placeholder="Ask AI..."
-        rows={5}
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-        className="aipicker-textarea"
-      />
-      <div className="flex flex-wrap gap-3">
-        {generatingImg ? (
-          <CustomButton
-            type="outline"
-            title="Asking AI..."
-            customStyles="text-xs"
-          />
-        ) : (
-          <>
+    <AnimatePresence>
+      <div className="aipicker-container ">
+        <button
+          className="absolute -top-7 -right-7 h-6 w-6 flex justify-center items-center rounded-full bg-red-600 text-2xl text-white border-2"
+          onClick={() => setActiveEditorTab("")}
+        >
+          <IoClose />
+        </button>
+        <textarea
+          placeholder="Ask AI..."
+          rows={5}
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          className="aipicker-textarea"
+        />
+        <div className="flex flex-wrap gap-3">
+          {generatingImg ? (
             <CustomButton
               type="outline"
-              title="AI Logo"
-              handleClick={() => handleSubmit("logo")}
+              title="Asking AI..."
               customStyles="text-xs"
             />
-            <CustomButton
-              type="filled"
-              title="AI Full"
-              handleClick={() => handleSubmit("full")}
-              customStyles="text-xs"
-            />
-          </>
-        )}
+          ) : (
+            <>
+              <CustomButton
+                type="outline"
+                title="AI Logo"
+                handleClick={() => handleSubmit("logo")}
+                customStyles="text-xs"
+              />
+              <CustomButton
+                type="filled"
+                title="AI Full"
+                handleClick={() => handleSubmit("full")}
+                customStyles="text-xs"
+              />
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </AnimatePresence>
   );
 };
 
